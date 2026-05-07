b96741c44c096d0f879ace8a4a6498632232a80b
dpthibeault99 <dpthibeault@email.neit.edu>
2026-05-07 14:36:14 -0400

    math no work

--- js/gameLoop.js
+++ js/gameLoop.js
@@ -5,15 +5,29 @@ var context = canvas.getContext("2d");
 var interval = 1000/60;
 var timer = setInterval(animate,interval);
 
-dot = new gameObject(100,100,50,50, "#ff0000" );
+var colors = [];
+colors[0] = "#ff0000"
+colors[1] = "#00ff00"
+colors[2] = "#0000ff"
 
+var amount = 50;
 
+var dots = [];
+for(var i = 0; i < amount ; i++)
+{
+    dots[i] = new gameObject();
+    dots[i].x = Math.random() * canvas.width; 
+    dots[i].y = Math.random() * canvas.height; 
+    dots[i].color = colors[Math.floor(rand(0,2.9))];
+    dots[i].width = rand(5,20);
 
-
-
+}
 
 function animate()
 {
     context.clearRect(0,0,canvas.width, canvas.height);
-    dot.drawCircle();
+    for(i=0; i < amount; i++)
+    {
+        dots[i].drawCircle();
+    }
 }
